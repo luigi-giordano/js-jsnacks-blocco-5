55eaf5238069493b5c0db9643221314de3faf19b
luigi-giordano <luigi.giordano1992@gmail.com>
2024-12-02 19:21:18 +0100

    add comments 2 snack

--- snack-2/script.js
+++ snack-2/script.js
@@ -6,5 +6,11 @@ const people = [
 
 const names = people.map(person => person.name);
 console.log(names.join(', '));
+// Prende ciascun oggetto (person) come input.
+// Restituisco solo la proprietà name dell'oggetto.
+
+// Dopo aver ottenuto l'array dei nomi, utilizziamo il metodo join per convertirlo in una stringa,
+// separando i nomi con una virgola e uno spazio (, ).
+
 // Stampa in console tutti i nomi
 // Risultato: 'Paolo', 'Giulia', 'Marco'
